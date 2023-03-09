6840ee7813079f6f1b56ad16e1692493db4f2d21
Elzewire <37222903+Elzewire@users.noreply.github.com>
2023-03-09 10:55:44 +0300

    L2 09.03 User class
    L2 09.03 User class

--- L2_09_03/src/Main.java
+++ L2_09_03/src/Main.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         User u1 = new User("login", "1234");
         User u2 = new User("login2", "4321");
 
+        /*
         Scanner sc = new Scanner(System.in);
         String login = sc.nextLine();
         String pwd = sc.nextLine();
 
         System.out.println(u1.checkLogin(login, pwd));
+        */
+
+        u1.addFriend(u2);
+        //u2.addFriend(u1);
+
+        System.out.println(u1.getFriends());
+        System.out.println(u2.getFriends());
     }
 }
--- L2_09_03/src/User.java
+++ L2_09_03/src/User.java
@@ -1,17 +1,61 @@
-import java.util.Base64;
+import java.util.ArrayList;
+import java.sql.Date;
+import java.util.List;
 
 public class User {
     private String login;
     private String pwd;
+    private String fio;
+    private Date birthdate;
+
+    private List<User> friends;
 
     public User(String login, String pwd) {
         this.login = login;
         this.pwd = pwd;
-        //this.pwd = Base64.getEncoder().encodeToString(pwd.getBytes());
+        friends = new ArrayList<>();
     }
 
     public boolean checkLogin(String login, String pwd) {
         return this.login.equals(login) && this.pwd.equals(pwd);
-                //this.pwd.equals(Base64.getEncoder().encodeToString(pwd.getBytes()));
+    }
+
+    public void addFriend(User friend) {
+        if (!friends.contains(friend)) {
+            friends.add(friend);
+            friend.addFriend(this);
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public List<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<User> friends) {
+        this.friends = friends;
     }
 }
